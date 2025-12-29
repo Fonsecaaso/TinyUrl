18be97a13285fc03137c0102e9be1f7603907a82
fonsecaaso <mateus.fonseca@topsort.com>
2025-12-29 20:02:49 -0300

    translate

--- go-server/scripts/load-test.js
+++ go-server/scripts/load-test.js
@@ -5,14 +5,14 @@ export const options = {
   stages: [
     { duration: '1m', target: 30 },
     { duration: '1m', target: 60 },
-    // { duration: '1m', target: 90 },
-    // { duration: '1m', target: 120 },
-    // { duration: '1m', target: 150 },
+    { duration: '1m', target: 90 },
+    { duration: '1m', target: 120 },
+    { duration: '1m', target: 150 },
     { duration: '1m', target: 50 },
   ],
   thresholds: {
-    http_req_duration: ['p(95)<800'], // ajuste conforme seu SLO
-    http_req_failed: ['rate<0.01'],   // <1% de erro
+    http_req_duration: ['p(95)<800'], // adjust according to my SLO
+    http_req_failed: ['rate<0.01'],   // <1% error
   },
 };
 
@@ -20,14 +20,14 @@ export default function () {
   const res = http.get('https://fonsecaaso.com/api/tL8AQC', {
     headers: {
       'User-Agent': 'k6-prod-test',
-      'X-K6-Test': 'true', // útil se quiser tratar no backend
+      'X-K6-Test': 'true', 
     },
-    redirects: 5, // importante se esse endpoint redirecionar
+    redirects: 5, 
   });
 
   check(res, {
-    'status é 200 ou 3xx': (r) => r.status === 200 || (r.status >= 300 && r.status < 400),
-    'tempo < 800ms': (r) => r.timings.duration < 800,
+    'status = 200 or 3xx': (r) => r.status === 200 || (r.status >= 300 && r.status < 400),
+    'response time < 800ms': (r) => r.timings.duration < 800,
   });
 
   sleep(0);
